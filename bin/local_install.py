1d15faeedeedbb6f9207e12e2ea48c8df41ea94c
Darcy Shen <66561265+da-tubi@users.noreply.github.com>
2021-12-16 14:38:05 +0800

    Fix typo (#33)

--- bin/local_install.py
+++ bin/local_install.py
@@ -13,7 +13,7 @@ version = "0.0.3"
 
 ffmpeg = f"https://repo1.maven.org/maven2/org/bytedeco/ffmpeg/4.4-1.5.6/ffmpeg-4.4-1.5.6-{classifier}.jar"
 javacpp = f"https://repo1.maven.org/maven2/org/bytedeco/javacpp/1.5.6/javacpp-1.5.6-{classifier}.jar"
-spark_video = "https://github.com/eto-ai/spark-video/releases/download/v{version}/spark-video-assembly_2.12-{version}.jar"
+spark_video = f"https://github.com/eto-ai/spark-video/releases/download/v{version}/spark-video-assembly_2.12-{version}.jar"
 
 subprocess.run(["wget", "-O", f"{spark_home}/jars/ffmpeg-{classifier}.jar", ffmpeg])
 subprocess.run(["wget", "-O", f"{spark_home}/jars/javacpp-{classifier}.jar", javacpp])
